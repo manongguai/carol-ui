a8c2d32204ca8d93bf1fd750ff1d44f6059dc9c5
kirk <jgbzql@163.com>
2024-07-31 12:22:27 +0800

    feat: 纵向滚动

--- packages/core/src/App.vue
+++ packages/core/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <cl-table style="width: 1000px; margin: 0 auto" :data="data">
+    <cl-table style="width: 1000px; margin: 0 auto; height: 200px" :data="data">
       <cl-table-column prop="name" label="Name"> </cl-table-column>
       <cl-table-column prop="age" label="Age"> </cl-table-column>
       <cl-table-column prop="name" label="Name"> </cl-table-column>
@@ -18,15 +18,99 @@
       <cl-table-column prop="age" label="Age"> </cl-table-column>
     </cl-table>
 
-    <cl-table style="width: 1000px; margin: 0 auto" :data="data">
+    <!-- <cl-table style="width: 1000px; margin: 0 auto" :data="data">
       <cl-table-column prop="name" label="Name"> </cl-table-column>
-    </cl-table>
+    </cl-table> -->
   </div>
 </template>
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
 import ClTable from '../packages/components/table'
 const data = ref([
+  {
+    name: 'Alice',
+    age: 25
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Alice',
+    age: 25
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Alice',
+    age: 25
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Alice',
+    age: 25
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Alice',
+    age: 25
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Alice',
+    age: 25
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Alice',
+    age: 25
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
+  {
+    name: 'Bob',
+    age: 30
+  },
   {
     name: 'Alice',
     age: 25
